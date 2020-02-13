c7e43f4691dd8fc3ea56074f558972b6c8258705
cs61b-aqn <travis.reyes@berkeley.edu>
2020-02-12 20:13:13 -0800

    confessions update sp20

--- priv/confessions/confessions.js
+++ priv/confessions/confessions.js
@@ -1,15 +1,4 @@
 confessions = [
-    "I need Pas'gym to start already I haven't been to the RSF for months now",
-    "If you want us to talk about new people, give us something new to talk about 🤧",
-    "GET TO KNOW THE NEW INTERNS DON'T BE PA'SHY :D",
-    "Travis do it don’t be a bitch",
-    "Travis just shoot your shot bro",
-    "no one's ever at the esh space when i'm there 🥺",
-    "https://www.when2meet.com/?8331848-YjjEi",
-    "pas'rave? ",
-    "My staffer yells at me for just existing. My staffer is abusive. To clarify, this is a call for help. SAVE ME",
-    "Intern meetings are fun because I get to know everyone better and watch some folx flirt with each other for 1.5 hours <3 ",
-    "AJ S YOU GOT ME WEAK IN THE KNEES 😩💦"
 ]
 
 const autowrite = () => {
