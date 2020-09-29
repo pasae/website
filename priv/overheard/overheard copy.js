e88474ca995b112a3b0abfc238cbc10556ec165a
connermanuel <manuel.conner.g@gmail.com>
2020-09-30 01:10:47 +0800

    update confessions and overheard

---
+++ priv/overheard/overheard copy.js
@@ -0,0 +1,44 @@
+overheard = [
+]
+
+const autowrite = () => {
+    let overheardToPost = '';
+
+    overheard.sort((a, b) => b.length - a.length)
+
+    for (let i in overheard) {
+        const quote = overheard[i].split("|");
+        if (quote.length == 1) {
+            let card = `
+            <div class="col-md-6">
+                <blockquote style="border-left: none">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            ${quote[0]}
+                        </ol>
+                    </nav>
+                </blockquote>
+            </div>`;
+            overheardToPost += card;
+        } else {
+            let card = `
+            <div class="col-md-6">
+                <blockquote style="border-left: none">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            ${quote[0]}
+                            <br>
+                            - ${quote[1]}
+                        </ol>
+                    </nav>
+                </blockquote>
+			</div>`;
+            overheardToPost += card;
+        }
+
+    }
+    console.log(overheardToPost)
+    document.getElementById("overheard").innerHTML = overheardToPost;
+}
+
+autowrite();
