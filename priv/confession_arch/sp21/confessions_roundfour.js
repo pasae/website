38d22044e9857068952837603d3e8fa0db15ca3d
Conner Manuel <manuel.conner.g@gmail.com>
2021-05-02 15:01:32 +0800

    confessions

---
+++ priv/confession_arch/sp21/confessions_roundfour.js
@@ -0,0 +1,14 @@
+confessions = [
+    "hey, it’s me again. just got my second vaccine shot and it got me wondering what would happen if we gave us another shot. anyway, i hope ur doing well. u don’t have to respond. be safe",
+    "The next webmaster should start a pas' dating section, who's in?",
+    "This is my first confession! hi!",
+    "Do you think that when we go back to being in-person next year, we can all have a big group hug? I miss y'all",
+    "bring back pas'shipping",
+    "You see, I've been hurt, a lot. And I'm tired of always opening up and always being the one to make the jump, only to fall on my face and break my own heart. So yes, I'm scared. I'm scared to put my heart on the line again, after just barely picking up the pieces and putting them back together again.... and maybe it's too much to ask or childish of me.... but if there's something, anything, say it first, Take one step toward me, because I really can't take anymore breaks. ",
+    "Rigel , can we go on a date? pls . thx. C u at the next meeting . PS I saw u on tinder <3",
+    "Camille ",
+    "Camille...",
+    "Camillle?",
+    "Camille, you haven't responded to my text... ",
+    "Conner , let's be friends !"
+]
--- priv/confessions/confessions.js
+++ priv/confessions/confessions.js
@@ -1,16 +1,9 @@
 confessions = [
-    "hey, it’s me again. just got my second vaccine shot and it got me wondering what would happen if we gave us another shot. anyway, i hope ur doing well. u don’t have to respond. be safe",
-    "The next webmaster should start a pas' dating section, who's in?",
-    "This is my first confession! hi!",
-    "Do you think that when we go back to being in-person next year, we can all have a big group hug? I miss y'all",
-    "bring back pas'shipping",
-    "You see, I've been hurt, a lot. And I'm tired of always opening up and always being the one to make the jump, only to fall on my face and break my own heart. So yes, I'm scared. I'm scared to put my heart on the line again, after just barely picking up the pieces and putting them back together again.... and maybe it's too much to ask or childish of me.... but if there's something, anything, say it first, Take one step toward me, because I really can't take anymore breaks. ",
-    "Rigel , can we go on a date? pls . thx. C u at the next meeting . PS I saw u on tinder <3",
-    "Camille ",
-    "Camille...",
-    "Camillle?",
-    "Camille, you haven't responded to my text... ",
-    "Conner , let's be friends !"
+    "rigel when r u gonna take me on a date :((",
+    "Avery's new hairstyle has me looking at him in a different light👀",
+    "avery may have experience in percussion, but i have something else he could bang...",
+    "Ali is literally so nice and so pretty what did we do to deserve her she's on a realm of her own she's ascended love that for u queen",
+    "love u core 33"
 ]
 
 const autowrite = () => {
