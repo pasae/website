4be7df87f45b259f4ed3e38251a25ca14dc7a531
Conner Manuel <manuel.conner.g@gmail.com>
2021-05-17 15:01:15 +0800

    last confessions

---
+++ priv/confession_arch/sp21/confessions_roundfive.js
@@ -0,0 +1,67 @@
+confessions = [
+    "rigel when r u gonna take me on a date :((",
+    "Avery's new hairstyle has me looking at him in a different light👀",
+    "avery may have experience in percussion, but i have something else he could bang...",
+    "Ali is literally so nice and so pretty what did we do to deserve her she's on a realm of her own she's ascended love that for u queen",
+    "love u core 33"
+]
+
+const autowrite = () => {
+    let confessionsToPost = '';
+
+    const cardColors = ['primary', 'danger', 'warning'];
+    let cardNum = 0;
+
+    confessions.sort((a, b) => b.length - a.length)
+
+    for (let i in confessions) {
+        const conf = confessions[i];
+        const color = cardColors[cardNum];
+        var size = 'h5'
+        if (conf.length > 100) {
+            size = "h6" }
+        let card = `
+        <div class="col-md-4">
+            <div class="card text-white bg-${color} mb-3" >
+                <div class="card-body">
+                    <${size} class="card-text">
+                        ${conf}
+                    </v>
+                </div>
+            </div>
+        </div>`
+        confessionsToPost += card;
+
+        if (cardNum == 2) {
+            cardNum = 0;
+        } else {
+            cardNum += 1;
+        }
+    }
+
+    // for (let i in confessions) {
+    //     const conf = confessions[i];
+    //     const color = cardColors[cardNum];
+    //     let card = `
+    //     <div class="col-md-4">
+    //         <div class="card text-white bg-${color} mb-3" >
+    //             <div class="card-body">
+    //                 <h5 class="card-text">
+    //                     ${conf}
+    //                 </h5>
+    //             </div>
+    //         </div>
+    //     </div>`
+    //     confessionsToPost += card;
+
+    //     if (cardNum == 2) {
+    //         cardNum = 0;
+    //     } else {
+    //         cardNum += 1;
+    //     }
+    // }
+
+    document.getElementById("confessions").innerHTML = confessionsToPost;
+}
+
+autowrite();
