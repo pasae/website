7d5ffe540b863b70ad26d811960b406165719415
Matthew Mojica <mojica.matthew02@gmail.com>
2022-08-13 14:32:57 -0700

    Debugging contact form

--- contact-3.php
+++ contact-3.php
@@ -3,8 +3,11 @@
 THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
 AND ALSO SMTP TO SEND THE EMAILS
 */
+
 namespace MyProject;
+
 use PHPMailer\PHPMailer\PHPmailer;
+
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 require 'PHPMailer/src/Exception.php';
@@ -51,10 +54,10 @@ try {
     if (count($_POST) == 0) {
         throw new \Exception('Form is empty');
     }
-    
+
     $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
     $emailTextHtml .= "<table>";
-    
+
     foreach ($_POST as $key => $value) {
         // If the field exists in the $fields array, include it in the email
         if (isset($fields[$key])) {
@@ -62,58 +65,58 @@ try {
         }
     }
     $emailTextHtml .= "</table><hr>";
-    $emailTextHtml .= "<p>Best,<br>Conner Manuel <br> PASAE 33 Webmaster</p>";
-    
+    $emailTextHtml .= "<p>Best,<br>Matthew Mojica <br> PASAE 35 Webmaster</p>";
+
     $mail = new PHPMailer;
-    
+
     $mail->setFrom($fromEmail, $fromName);
     $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
     $mail->addReplyTo($from);
-    
+
     $mail->isHTML(true);
-    
+
     $mail->Subject = $subject;
     $mail->Body    = $emailTextHtml;
     $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
-    
-    
+
+
     $mail->isSMTP();
-    
+
     //Enable SMTP debugging
     // 0 = off (for production use)
     // 1 = client messages
     // 2 = client and server messages
-    $mail->SMTPDebug = 1;
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->Debugoutput = 'html';
-    
+
     //Set the hostname of the mail server
     // use
     // $mail->Host = gethostbyname('smtp.gmail.com');
     // if your network does not support SMTP over IPv6
     $mail->Host = $smtpHost;
-    
+
     //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
     $mail->Port = 587;
-    
+
     //Set the encryption system to use - ssl (deprecated) or tls
     $mail->SMTPSecure = 'tls';
 
     //Set the authentication type
     $mail->AuthType = 'LOGIN';
-    
+
     //Whether to use SMTP authentication
     $mail->SMTPAuth = true;
-    
+
     //Username to use for SMTP authentication - use full email address for gmail
     $mail->Username = $smtpUsername;
-    
+
     //Password to use for SMTP authentication
     $mail->Password = $smtpPassword;
-    
+
     if (!$mail->send()) {
         throw new \Exception('I could not send the email. ' . $mail->ErrorInfo);
     }
-    
+
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 } catch (\Exception $e) {
     // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
@@ -124,9 +127,9 @@ try {
 // if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
-    
+
     header('Content-Type: application/json');
-    
+
     echo $encoded;
 }
 // else just display the message
