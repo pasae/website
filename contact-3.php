b17631c1583656b5cbce524069e8f1c3d96e2169
Matthew Mojica <mojica.matthew02@gmail.com>
2022-08-19 19:22:58 -0700

    Debugging contact form - smtpdebug 3

--- contact-3.php
+++ contact-3.php
@@ -86,7 +86,7 @@ try {
     // 0 = off (for production use)
     // 1 = client messages
     // 2 = client and server messages
-    $mail->SMTPDebug = 2;
+    $mail->SMTPDebug = 3;
     $mail->Debugoutput = 'html';
 
     //Set the hostname of the mail server
