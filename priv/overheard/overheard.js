74d017a723c9bf9e79aa21e68c22205d4ec26828
Adriel Vijuan <avijuan@berkeley.edu>
2023-09-26 18:19:57 -0700

    confessions round 2, overheard round 1

--- priv/overheard/overheard.js
+++ priv/overheard/overheard.js
@@ -1,48 +1,68 @@
-overheard = [
-"pa'sayonara - :| garrett matt carm "
-"asthma hookah - anonymous"
-"None of my business - anonymous"
-"I like 'em segregated. - talking about different colored clothes"
-]
+console.log("Script loaded");
 
-const autowrite = () => {
-    let overheardToPost = '';
+document.addEventListener("DOMContentLoaded", function () {
+    const overheard = [
+        "'pa'sayonara' - :| garrett matt carm ",
+        "'asthma hookah'",
+        "'None of my business'",
+        "'I like 'em segregated.' - talking about different colored clothes",
+        "'2.5 inches is too big!'",
+        "'my ex made a diss track about me with 1M streams on Spotify boohoo: ((' - guess",
+        "'DO YOU SEE MY VISION OR WHAT'",
+        "'    this article is so verbacious'",
+        "'it's fickle bro' - matt",
+        "'gee eye eff' - thomas",
+        "'i have to solve climate change by 5pm'",
+        "'    Dont punch me…Im sensitive. 🥺' - Freya",
+        "'I don't even need to see, to see bro' - Garrett",
+        "'I need to call Joaquin so he can walk me home.Joaquin be Joaquin(walking) ? '",
+        "'Showerclap * clap clap shhhhhh * WE'RE PLAYING RAGE CAGE'",
+        "'don't worry you're cracked'",
+        "'hee hee hee har' - Cody (real)",
+        "'What's 30 times 2? Oh 90' - Ate Karlaine",
+    ];
 
-    overheard.sort((a, b) => b.length - a.length)
+    const autowrite1 = () => {
+        let overheardToPost = '';
 
-    for (let i in overheard) {
-        const quote = overheard[i].split("|");
-        if (quote.length == 1) {
+        const cardColors = ['primary', 'danger', 'warning'];
+        let cardNum = 0;
+
+        overheard.sort((a, b) => b.length - a.length);
+
+        for (let i in overheard) {
+            const conf = overheard[i];
+            const color = cardColors[cardNum];
+            var size = 'h5';
+            if (conf.length > 100) {
+                size = "h6";
+            }
             let card = `
-            <div class="col-md-6">
-                <blockquote style="border-left: none">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            ${quote[0]}
-                        </ol>
-                    </nav>
-                </blockquote>
+            <div class="col-md-4">
+                <div class="card text-white bg-${color} mb-3" >
+                    <div class="card-body">
+                        <${size} class="card-text">
+                            ${conf}
+                        </${size}>
+                    </div>
+                </div>
             </div>`;
             overheardToPost += card;
-        } else {
-            let card = `
-            <div class="col-md-6">
-                <blockquote style="border-left: none">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            ${quote[0]}
-                            <br>
-                            - ${quote[1]}
-                        </ol>
-                    </nav>
-                </blockquote>
-			</div>`;
-            overheardToPost += card;
+
+            if (cardNum == 2) {
+                cardNum = 0;
+            } else {
+                cardNum += 1;
+            }
         }
 
+        const overheardElement = document.getElementById("overheard");
+        if (overheardElement) {
+            overheardElement.innerHTML = overheardToPost;
+        }
+    
+    
     }
-    console.log(overheardToPost)
-    document.getElementById("overheard").innerHTML = overheardToPost;
+    autowrite1();
 }
-
-autowrite();
+);
