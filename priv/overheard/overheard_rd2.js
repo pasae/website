18fb53421b3478c9a49fff8f2150c608202d135e
Breyes5 <brendanreyes1104@gmail.com>
2026-03-06 19:58:25 -0800

    overheards wk2

--- priv/overheard/overheard_rd2.js
+++ priv/overheard/overheard_rd2.js
@@ -2,27 +2,12 @@ console.log("Script loaded");
 
 document.addEventListener("DOMContentLoaded", function () {
     const overheard = [
-        "Do Chinese people have this? Do they have a CHASAE? -Matthew",
-        "i heard garret doesnt shower ewww grossss yuck",
-        "im not lettin no boy eat my cum",
-        "shout out my trans folk -Xavier",
-        "i don’t hate gay michael, i hate taiwanese dan! -Garrett",
-        "cum in my butt -Shane",
-        "but my income !!!!!!!!",
-        "peach! couple to throuple is my favorite show!",
-        "I didn't mean to zoom in on his balls my bad -Francesca",
-        "Are you comfortable getting on your knees? -Matthew",
-        `(on the pasae instagram search bar)
-
-        "sourdough basics"
-        "sourdough"
-        "how to dress basics men"
-        "how to dress better men"`,
-        "I want to hear you guys moan -Albie",
-        "I wouldn't want to be named Pittsburgh -Xavier",
-        "Everyone wants skinless chicken, but what about skinful chicken? -Garrett",
-        "Take it from the goose -Francesca",
-        "Did you feel like a loser back then"
+        "I love coc -Shane",
+        "lemme crush those nuts -Christian",
+        "you should always remember if you ever put your mind to it you can do this -Matthew",
+        "Who hasn't twerked let's be honest -Brendan",
+        "Yeah I like my girlfriend she's like my sister",
+        "Let's load the penis on the plate -Xavier"
     ];
 
     const autowrite1 = () => {
